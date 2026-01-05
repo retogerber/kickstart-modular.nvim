c05ef31dd5af75eaa5c303bd3e1e10f20f2a4276
retogerber <retgerb@gmail.com>
2026-01-05 13:01:21 +0100

    add iron command

--- lua/custom/plugins/iron.lua
+++ lua/custom/plugins/iron.lua
@@ -3,10 +3,19 @@ return {
     'Vigemus/iron.nvim',
     config = function()
       local iron = require 'iron.core'
+      local common = require 'iron.fts.common'
+      local bracketed_paste_quarto = function(lines, extras)
+        lines = common.bracketed_paste_python(lines, extras)
+        table.remove(lines, 1)
+        table.insert(lines, ' ')
+        return lines
+      end
       iron.setup {
         config = {
           -- Whether a repl should be discarded or not
           scratch_repl = true,
+          -- Automatically closes the repl window on process end
+          close_window_on_exit = true,
           -- Your repl definitions come here
           repl_definition = {
             R = {
@@ -21,6 +30,12 @@ return {
             },
             quarto = {
               command = { 'R' },
+              block_dividers = {
+                '```{python}',
+                '```{r}',
+                '```',
+              },
+              format = bracketed_paste_quarto,
             },
           },
           -- How the repl window will be displayed
@@ -30,16 +45,19 @@ return {
         -- iron doesn't set keymaps by default anymore.
         -- You can set them here or manually add keymaps to the functions in iron.core
         keymaps = {
+          toggle_repl = '<localleader>rr', -- toggles the repl open and closed.
           send_motion = '<localleader>R',
           visual_send = '<localleader>r<cr>',
           send_file = '<localleader>rf<cr>',
           send_line = '<localleader>rl',
           send_paragraph = '<localleader>rp',
           send_until_cursor = '<localleader>rU<cr>',
-          -- send_mark = "<localleader>rm",
-          -- mark_motion = "<localleader>mc",
-          -- mark_visual = "<localleader>mc",
-          -- remove_mark = "<localleader>md",
+          send_code_block = '<localleader>rb',
+          send_code_block_and_move = '<localleader>rn',
+          -- send_mark = '<localleader>rm',
+          -- mark_motion = '<localleader>mc',
+          -- mark_visual = '<localleader>mc',
+          -- remove_mark = '<localleader>md',
           cr = '<localleader>rA<cr>',
           interrupt = '<localleader>r<space>',
           exit = '<localleader>rQ',
@@ -56,6 +74,17 @@ return {
       vim.keymap.set('n', '<localleader>rR', '<cmd>IronRestart<cr>', { desc = '[R]EPL [R]estart' })
       vim.keymap.set('n', '<localleader>rF', '<cmd>IronFocus<cr>', { desc = '[R]EPL [F]ocus' })
       vim.keymap.set('n', '<localleader>rH', '<cmd>IronHide<cr>', { desc = '[R]EPL [H]ide' })
+      vim.keymap.set('n', '<localleader>rm', function()
+        local found = vim.fn.search('```{', 'bW')
+        if found == 0 then
+          vim.fn.search('```{', 'W')
+        end
+        iron.send_code_block(true)
+        vim.defer_fn(function()
+          vim.fn.search('```{', 'W')
+          vim.cmd 'normal! j'
+        end, 20)
+      end, { desc = 'Execute code block + jump to next ```' })
     end,
   },
 }
--- setup.sh
+++ setup.sh
@@ -2,46 +2,46 @@
 #
 
 mkdir -p /home/$USER/.local/bin
-#
-#export PATH=$PATH:/home/$USER/.local/bin
-#echo current path: $PATH
-#echo add 'export PATH=$PATH:/home/$USER/.local/bin' to .bashrc
-#
-#cd /home/$USER/.local
-#
-## neovim
-#mkdir -p nvim
-#cd nvim
-#wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-#tar -xf nvim-linux-x86_64.tar.gz
-#cd /home/$USER/.local/bin
-#ln -s /home/$USER/.local/nvim/nvim-linux-x86_64/bin/nvim nvim
 
-## ripgrep
-#mkdir -p /home/$USER/.local/ripgrep
-#cd /home/$USER/.local/ripgrep
-#wget https://github.com/BurntSushi/ripgrep/releases/download/15.1.0/ripgrep-15.1.0-x86_64-unknown-linux-musl.tar.gz
-#tar -xf ripgrep-15.1.0-x86_64-unknown-linux-musl.tar.gz
-#cd /home/$USER/.local/bin
-#ln -s /home/$USER/.local/ripgrep/ripgrep-15.1.0-x86_64-unknown-linux-musl/rg rg
+export PATH=$PATH:/home/$USER/.local/bin
+echo current path: $PATH
+echo add 'export PATH=$PATH:/home/$USER/.local/bin' to .bashrc
+
+cd /home/$USER/.local
+
+# neovim
+mkdir -p nvim
+cd nvim
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+tar -xf nvim-linux-x86_64.tar.gz
+cd /home/$USER/.local/bin
+ln -s /home/$USER/.local/nvim/nvim-linux-x86_64/bin/nvim nvim
+
+# ripgrep
+mkdir -p /home/$USER/.local/ripgrep
+cd /home/$USER/.local/ripgrep
+wget https://github.com/BurntSushi/ripgrep/releases/download/15.1.0/ripgrep-15.1.0-x86_64-unknown-linux-musl.tar.gz
+tar -xf ripgrep-15.1.0-x86_64-unknown-linux-musl.tar.gz
+cd /home/$USER/.local/bin
+ln -s /home/$USER/.local/ripgrep/ripgrep-15.1.0-x86_64-unknown-linux-musl/rg rg
 
 # fd-find
-#mkdir -p /home/$USER/.local/fd-find
-#cd /home/$USER/.local/fd-find
-#wget https://github.com/sharkdp/fd/releases/download/v10.3.0/fd-v10.3.0-x86_64-unknown-linux-musl.tar.gz
-#tar -xf fd-v10.3.0-x86_64-unknown-linux-musl.tar.gz
-#cd /home/$USER/.local/bin
-#ln -s /home/$USER/.local/fd-find/fd-v10.3.0-x86_64-unknown-linux-musl/fd fd
+mkdir -p /home/$USER/.local/fd-find
+cd /home/$USER/.local/fd-find
+wget https://github.com/sharkdp/fd/releases/download/v10.3.0/fd-v10.3.0-x86_64-unknown-linux-musl.tar.gz
+tar -xf fd-v10.3.0-x86_64-unknown-linux-musl.tar.gz
+cd /home/$USER/.local/bin
+ln -s /home/$USER/.local/fd-find/fd-v10.3.0-x86_64-unknown-linux-musl/fd fd
 
 # clipboard
 # https://askubuntu.com/a/1486875
 
 # luarocks
-#mkdir -p /home/$USER/.local/luarocks
-#cd /home/$USER/.local/luarocks
-#wget https://luarocks.github.io/luarocks/releases/luarocks-3.12.2-linux-x86_64.zip
-#unzip luarocks-3.12.2-linux-x86_64.zip
-#cd /home/$USER/.local/bin
+mkdir -p /home/$USER/.local/luarocks
+cd /home/$USER/.local/luarocks
+wget https://luarocks.github.io/luarocks/releases/luarocks-3.12.2-linux-x86_64.zip
+unzip luarocks-3.12.2-linux-x86_64.zip
+cd /home/$USER/.local/bin
 #ln -s /home/$USER/.local/luarocks/luarocks-3.12.2-linux-x86_64/luarocks luarocks
 #
 
@@ -49,18 +49,18 @@ mkdir -p /home/$USER/.local/bin
 #git clone https://github.com/retogerber/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
 
 # air (R lsp)
-#mkdir -p /home/$USER/.local/air
-#cd /home/$USER/.local/air
-#curl -LsSf https://github.com/posit-dev/air/releases/latest/download/air-installer.sh | sh
+mkdir -p /home/$USER/.local/air
+cd /home/$USER/.local/air
+curl -LsSf https://github.com/posit-dev/air/releases/latest/download/air-installer.sh | sh
 
 # treesitter cli
-#mkdir -p /home/$USER/.local/treesitter-cli
-#cd /home/$USER/.local/treesitter-cli
-#wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.26.3/tree-sitter-linux-x64.gz
-#gunzip tree-sitter-linux-x64.gz
-#chmod +x tree-sitter-linux-x64
-#cd /home/$USER/.local/bin
-#ln -s /home/$USER/.local/treesitter-cli/tree-sitter-linux-x64 tree-sitter-cli
+mkdir -p /home/$USER/.local/treesitter-cli
+cd /home/$USER/.local/treesitter-cli
+wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.26.3/tree-sitter-linux-x64.gz
+gunzip tree-sitter-linux-x64.gz
+chmod +x tree-sitter-linux-x64
+cd /home/$USER/.local/bin
+ln -s /home/$USER/.local/treesitter-cli/tree-sitter-linux-x64 tree-sitter-cli
 
 # quarto
 mkdir -p /home/$USER/.local/quarto
