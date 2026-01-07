883da5b5d98e94c227c54fd2dcc91a590bca95d5
retogerber <retgerb@gmail.com>
2026-01-07 13:18:14 +0100

    init obsidian

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -9,10 +9,27 @@ return {
       legacy_commands = false, -- this will be removed in the next major release
       workspaces = {
         {
-          name = 'personal',
+          name = 'work',
           path = '~/.config/nvim/obsidian_test',
         },
       },
+      daily_notes = {
+        folder = '.',
+        date_format = '%Y/%m/%Y_%m_%d',
+        alias_format = '%dth %B %Y',
+        default_tags = { 'type/daily-notes' },
+        template = 'templates/Daily_template.md',
+        workdays_only = true,
+      },
+      templates = {
+        folder = 'templates',
+        date_format = '%A %dth %B %Y',
+        time_format = '%H:%M',
+      },
     },
+    -- vim.keymap.set('n', '<localleader>on', function()
+    --   local api = require 'obsidian.api'
+    --   api.new_from_template('id', '/Daily_template.md')
+    -- end, { desc = 'Execute code block + jump to next ```' }),
   },
 }
---
+++ obsidian_test/templates/Daily_template.md
@@ -0,0 +1,21 @@
+---
+id: Daily_template
+aliases: []
+tags: []
+---
+
+# #Project1
+
+- [ ]
+
+
+
+## #Time_Tracking
+
+| Start | Stop | Project |
+|--------|--------|
+| {{time}} |  |  |
+|  |  |  |
+|  |  |  |
+
+
